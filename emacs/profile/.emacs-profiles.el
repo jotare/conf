f45e34e0351a58312623bbc533a0fdc907dccc4e
Joan Antoni RE <joanantoni.re16@gmail.com>
2022-12-08 21:48:28 +0100

    Add new profile: spacemacs. Spacemacs base installed

--- emacs/profile/.emacs-profiles.el
+++ emacs/profile/.emacs-profiles.el
@@ -7,5 +7,9 @@
   ("newbie" . ((user-emacs-directory . "~/.emacs.d.shims/newbie/newbie.d")
                (user-init-file . "~/.emacs.d.shims/newbie/newbie.d/init.el")))
 
+  ;; Spacemacs
+  ("spacemacs" . ((user-emacs-directory . "~/.emacs.d.shims/spacemacs/sources")
+                  (env . (("SPACEMACSDIR" . "~/.emacs.d.shims/spacemacs/spacemacs.d")))))
+
   ;; Vanilla emacs (white canvas to start new configurations)
   ("vanilla" . ((user-emacs-directory . "~/.emacs.d.shims/vanilla/vanilla.d"))))
