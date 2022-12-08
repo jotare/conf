25fd6969e3d19f41c8b6ac144f9a0cf1121dbe21
Joan Antoni RE <joanantoni.re16@gmail.com>
2022-12-08 15:24:10 +0100

    Add new profile: newbie. Newbie config originally for iTIC students

--- emacs/profile/.emacs-profiles.el
+++ emacs/profile/.emacs-profiles.el
@@ -1,3 +1,7 @@
 (
+  ;; Newbie configuration for iTIC students
+  ("newbie" . ((user-emacs-directory . "~/.emacs.d.shims/newbie/newbie.d")
+               (user-init-file . "~/.emacs.d.shims/newbie/newbie.d/init.el")))
+
   ;; Vanilla emacs (white canvas to start new configurations)
   ("vanilla" . ((user-emacs-directory . "~/.emacs.d.shims/vanilla/vanilla.d"))))
